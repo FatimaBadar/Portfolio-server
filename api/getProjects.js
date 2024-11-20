f4a29e7774964f59cebd48cc77c6402d64dbc140
Noor Fatima <fatimabadarq@gmail.com>
2024-11-20 21:27:58 +1300

    Fixed API routes and CORS

--- api/getProjects.js
+++ api/getProjects.js
@@ -1,14 +1,16 @@
+import express from 'express';
 import conn from '../dbConfig.js';
 
-export default async function handler(req, res) {
-    if (req.method === 'GET') {
-        const result = await conn`SELECT * from projects`;
-        if (result) {
-            return res.status(200).json({ message: 'Fetched Project data successfully', data: result });
-        } else {
-            return res.status(500).json({ message: 'Couldn\'t fetch Project data' });
-        }
-    } else {
-        return res.status(405).json({ message: 'Method Not Allowed' });
-    }
-}
+const router = express.Router();
+router.get('/', async (req, res) => {
+    try {
+        const result = await conn`SELECT * FROM projects`;
+        return res.status(200).json({
+          message: 'Fetched projects successfully',
+          data: result,
+        });
+      } catch (error) {
+        return res.status(500).json({ message: 'Error fetching projects', error: error.message });
+      }
+});
+export default router;
--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import bodyParser from 'body-parser';
-import conn from './dbConfig.js'
 import cors from 'cors'
 import dotenv from "dotenv";
-import transporter from './emailConfig.js';
+import contactMe from './api/contactMe.js';
+import getProjects from './api/getProjects.js'
 
 dotenv.config();
 const app = express();
@@ -16,52 +16,9 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json()); //Middleware
 
-app.get('/api/getProjects', async (req, res) => {
-    const result = await conn`SELECT * from projects`;
-    if(result){
-        return res.status(200).json({message: 'Fetched Project data successfully', data: results})
-    }
-    else{
-        return res.status(500).json({message: 'Couldnt fetched Project data'})
-    }
-    //  conn('SELECT * from projects', (err, results) => {
-    //     if(err){
-    //         console.error(err.message)
-    //         return res.status(500).json({message: 'Couldnt fetched Project data'})
-    //     }
-    //     return res.status(200).json({message: 'Fetched Project data successfully', data: results})
-    // })
-})
-
-app.post('/api/contactMe', (req, res) => {
-    const firstname = req.body.EmailPayload.firstname;
-    const lastname = req.body.EmailPayload.lastname;
-    const email = req.body.EmailPayload.email;
-    const message = req.body.EmailPayload.message;
-    
-    if(req.body.EmailPayload){
-        console.log("Received data:", req.body.EmailPayload);
-
-        //Send email       
-        let mailOptions = {
-            from: email, 
-            to: process.env.EMAIL_USER,
-            subject: 'Portfolio contact form', 
-            text: `From: ${firstname} ${lastname}:\n ${message}`, 
-        };
-        
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return res.status(500).json({ message: `Error: ${error}`});
-            }
-            return res.status(200).json({ message: `Message Sent: ${info.response}` });
-        });        
-    }
-    else{
-        return res.status(500).json({ message: 'No data in contact form' });
-    }
-
-})
+//API routes
+app.use('/api/contactMe', contactMe); 
+app.use('/api/getProjects', getProjects); 
 
 app.listen(process.env.PORT);
 console.log("server is running")
