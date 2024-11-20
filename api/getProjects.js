11c4b777e15ed6e7012e87811d080e7ebb1b6e82
Noor Fatima <fatimabadarq@gmail.com>
2024-11-20 21:53:07 +1300

    CORS resolved

--- api/getProjects.js
+++ api/getProjects.js
@@ -1,6 +1,10 @@
 import conn from "../dbConfig.js";
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+
   if (req.method === "GET") {
     try {
       const result = await conn`SELECT * FROM projects`;
