08506ce4948de6f1b7764c7a5d56552c3bda884e
Noor Fatima <fatimabadarq@gmail.com>
2025-01-23 19:43:27 +1300

    API

--- api/contactMe.js
+++ api/contactMe.js
@@ -6,30 +6,28 @@ dotenv.config();
 const router = express.Router()
 
 router.post('/', async (req, res) => {
-    const firstname = req.body.EmailPayload.firstname;
-    const lastname = req.body.EmailPayload.lastname;
-    const email = req.body.EmailPayload.email;
-    const message = req.body.EmailPayload.message;
+  const firstname = req.body.EmailPayload.firstname;
+  const lastname = req.body.EmailPayload.lastname;
+  const email = req.body.EmailPayload.email;
+  const message = req.body.EmailPayload.message;
 
-    if (req.body.EmailPayload) {
-      let mailOptions = {
-        from: email,
-        to: process.env.EMAIL_USER,
-        subject: "Portfolio contact form",
-        text: `From: ${firstname} ${lastname}:\n ${message}`,
-      };
+  if (!firstname || !lastname || !email || !message) {
+    return res.status(400).json({ message: "Invalid or missing contact form data" });
+  }
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_USER,
+    subject: "Portfolio contact form",
+    text: `From: ${firstname} ${lastname} (${email}):\n\n   ${message}`,
+  };
 
-      try {
-        const info = await transporter.sendMail(mailOptions);
-        return res
-          .status(200)
-          .json({ message: `Message Sent: ${info.response}` });
-      } catch (error) {
-        return res.status(500).json({ message: `Error: ${error.message}` });
-      }
-    } else {
-      return res.status(500).json({ message: "No data in contact form" });
-    }
-  });
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    return res.status(200).json({
+      message: `Message Sent: ${info.response}`});
+  } catch (error) {
+    return res.status(500).json({ message: `Error: ${error.message}` });
+  }
+});
 
 export default router;
